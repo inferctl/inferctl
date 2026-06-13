24bd4db187b5e52f8db5068c0bccefb45e2e3f1b
Dave Fobare <dave.fobare@gmail.com>
2026-06-13 02:10:20 -0400

    Normalize contract golden diff line endings

--- scripts/check-contract-goldens.sh
+++ scripts/check-contract-goldens.sh
@@ -12,7 +12,9 @@ cd "$ROOT"
 go build -o "$TMP/infer" ./cmd/infer
 
 "$TMP/infer" capabilities --json | jq .data >"$TMP/capabilities.golden.json"
-diff -u internal/contract/capabilities.golden.json "$TMP/capabilities.golden.json"
+tr -d '\r' <internal/contract/capabilities.golden.json >"$TMP/capabilities.expected.json"
+tr -d '\r' <"$TMP/capabilities.golden.json" >"$TMP/capabilities.actual.json"
+diff -u "$TMP/capabilities.expected.json" "$TMP/capabilities.actual.json"
 
 find testdata/contract -name '*.golden.json' -print0 | sort -z | while IFS= read -r -d '' file; do
   jq empty "$file"
